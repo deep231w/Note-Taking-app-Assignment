23a4fe197c71b92f5d13a392ec893d91ac568204
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-03-27 16:31:08 +0530

    initialized MUI

---
+++ components/theme.ts
@@ -0,0 +1,15 @@
+"use client"
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2', // Blue color
+    },
+    secondary: {
+      main: '#dc004e', // Red color
+    },
+  },
+});
+
+export default theme;
