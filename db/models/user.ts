605fdc88d836f9a2f914ad1776b382674ff58eee
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-03-29 01:31:38 +0530

    user model updated

--- db/models/user.ts
+++ db/models/user.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema=new mongoose.Schema({
+    name:{type:String, require:true},
     email:{type:String, require:true, unique:true},
     password:{type:String, require:true}
 })
