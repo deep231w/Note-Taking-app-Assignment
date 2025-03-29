1aa9d4561fe0fe2ee35e3c0c746d83bd99548113
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-03-29 12:05:52 +0530

    updated

--- app/api/user/signup/route.ts
+++ app/api/user/signup/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { User } from "@/db/models/user";
 import { connectDB } from "@/lib/db";
+import bcrypt from "bcryptjs";
+
 export async function POST(req:NextRequest) {
     try{
         await connectDB();
@@ -19,10 +21,12 @@ export async function POST(req:NextRequest) {
                 message:"user alredy exists , please try again with another email"
             })
         }
+        const hashedPassword= await bcrypt.hash(password, 10);
+
         const user= await User.create({
             name,
             email,
-            password
+            password:hashedPassword
         })
         return NextResponse.json({
             message:"signup successfully",
