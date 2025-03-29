d05d6c4a58b7a5f5e08e082b38c297e8b89871cc
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-03-29 13:40:37 +0530

    redirect issue fixed

--- utils/signUpAuth.ts
+++ utils/signUpAuth.ts
@@ -8,14 +8,15 @@ export const SignUp =async (name:string, email:string, password:string)=>{
         },{
             withCredentials:true,
         })
-        if(response.status==200){
+        if(response.status==201){
             const {user}= response.data;
             localStorage.setItem("user", JSON.stringify(user));
             console.log("user -",user);
             return response;
         }
-        
+        return response;
     }catch(e){
         console.log("erroe during signup:",SignUp);
+        return null
     }
 }
